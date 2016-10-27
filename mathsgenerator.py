cd5fd6af415717719ebe98a71f20a175a62253c8
Martin Hammer <martin.hammer@outlook.com>
2016-10-27 06:14:22 +0400

    Added multiplication operation and the proportion of operations can be adjusted

--- mathsgenerator.py
+++ mathsgenerator.py
@@ -6,6 +6,9 @@ import random
 # how many rows to generate
 outputRows = 36 
 
+# mix of operations from which a random selection will be made; proportions can be adjusted, e.g. ++- to get twice as many additions than subtractions
+operandMix = '+-*'
+
 # highest number for additions (left hand size)
 maxSummandLeft = 20
 
@@ -17,6 +20,12 @@ maxMinuend = 20
 
 # highest number for subtraction right hand side (subtrahend)
 maxSubtrahend = 10
+
+# highest number for multiplication left hand side (multiplicant)
+maxMultiplicant = 10
+
+# highest number for multiplication right hand side (multiplier)
+maxMultiplier = 10
 
 # maximum number of zeroes allowed to be generated (too many zeroes make the exercise too easy :))
 maxZeros = 2
@@ -32,9 +41,9 @@ def gimmeNumber( minValue, maxValue ):
 	global maxZeros
 	global countZeros
 
-	randomNumber = random.randrange( minValue, maxValue )
+	randomNumber = random.randrange( minValue, maxValue + 1 )
 	while ( randomNumber == 0 ) and ( countZeros >= maxZeros ):
-		randomNumber = random.randrange( minValue, maxValue )
+		randomNumber = random.randrange( minValue, maxValue + 1 )
 
 	return randomNumber
 
@@ -44,7 +53,7 @@ def gimmeNumber( minValue, maxValue ):
 for i in range( outputRows ):
 
 	# randomly decide kind of problem to generate
-	operand = random.choice( '+-' )
+	operand = random.choice( operandMix )
 
 	# generate numbers for an addition problem
 	if operand == '+':
@@ -64,9 +73,18 @@ for i in range( outputRows ):
 		right = gimmeNumber( 0, maxSubtrahend if ( left + 1 ) > maxSubtrahend else left + 1 )
 		if right == 0: countZeros = countZeros + 1
 
+	elif operand == '*':
+
+		left = gimmeNumber( 0, maxMultiplicant )
+		if left == 0: countZeros = countZeros + 1
+
+		right = gimmeNumber( 0, maxMultiplier )
+		if right == 0: countZeros = countZeros + 1
+
 	# other operations may be supported in the future!
 	else:
-		print "Sorry, something went wrong."
+		print( 'Sorry, {} operation is not supported.' ).format( operand )
+		next
 
 	# this is the output of one problem to solve
 	print( '{} {} {} =' ).format( left, operand, right )
